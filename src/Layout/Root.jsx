61c4fb929de05f745dda6ccc9200173bcf0e80bd
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-10-28 21:51:43 +0600

    Header Section Done

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom"
+import Header from "../components/Header"
 
 const Root = () => {
   return (
     <div>
+      <header>
+        <Header></Header>
+      </header>
       <Outlet></Outlet>
     </div>
   )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@
 const Home = () => {
     return (
         <div>
-            <h1 className="font-poppins">This is Home Page.</h1>
+            
         </div>
     )
 }
