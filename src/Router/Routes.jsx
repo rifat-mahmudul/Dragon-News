8bb982e365a158252777606e4ab240618b91d1fe
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-10-29 23:18:09 +0600

    Details News Done

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -5,6 +5,7 @@ import Root from "../Layout/Root";
 import Home from "../Pages/Home";
 import About from "../Pages/About";
 import Career from "../Pages/Career";
+import DetailsNews from "../components/DetailsNews";
 
 export const router = createBrowserRouter([
     {
@@ -22,6 +23,11 @@ export const router = createBrowserRouter([
             {
                 path : '/career',
                 element : <Career></Career>,
+            },
+            {
+                path : '/detailsNews/:id',
+                element : <DetailsNews></DetailsNews>,
+                loader : () => fetch("../news.json")
             },
         ]
     },
---
+++ src/components/DetailsNews.jsx
@@ -0,0 +1,31 @@
+import { Link, useLoaderData, useNavigate, useParams } from "react-router-dom"
+import RightSideNews from "./RightSideNews";
+
+const DetailsNews = () => {
+
+    const {_id} = useParams();
+    const newses = useLoaderData();
+    const news = newses.find(news => news._id == _id);
+    console.log(news);
+
+    const {image_url,title, details} = news;
+
+    return (
+        <div className="max-w-[90%] xl:max-w-[1150px] mx-auto grid lg:grid-cols-5 gap-8 mt-10 pb-16">
+            <div className="col-span-4">
+                <img src={image_url} alt="" />
+                <h1 className="font-bold text-2xl mt-3 mb-3">{title}</h1>
+                <p>{details}</p>
+                <Link to='/'>
+                    <button className="bg-blue-600 py-3 px-10 rounded-md text-white font-semibold mt-4">Back to All News</button>
+                </Link>
+            </div>
+
+            <div>
+                <RightSideNews></RightSideNews>
+            </div>
+        </div>
+    )
+}
+
+export default DetailsNews
